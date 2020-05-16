9743afdfe18d792c57b80e659cc871a9d7df25b2
Ajay <akverma@gwynniebee.com>
2020-05-16 21:30:56 +0530

    Updates

---
+++ precache-manifest.fa0bb6d28306bd141bc08b3f8549d371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afb296861d2e5f718d8f99108faa0c8",
+    "url": "/search-app-in-React/index.html"
+  },
+  {
+    "revision": "7fe2ff24988032b0ffdf",
+    "url": "/search-app-in-React/static/css/main.8a733bb6.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/search-app-in-React/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-app-in-React/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe2ff24988032b0ffdf",
+    "url": "/search-app-in-React/static/js/main.95200696.chunk.js"
+  },
+  {
+    "revision": "efb5d97583f8c5ddac8d",
+    "url": "/search-app-in-React/static/js/runtime-main.90587a84.js"
+  }
+]);
